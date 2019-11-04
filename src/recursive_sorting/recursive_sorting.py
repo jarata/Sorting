5ff553b23a9b2482bc3262232e3dfda7f1551485
Justin A <jarata@gmail.com>
2019-11-04 08:44:02 -0800

    Reset

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,41 +1,15 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
+def merge( arrA, arrB ):
+    elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    a = 0
-    b = 0
-    for i in range(0, elements):
-        if a >= len(arrA):
-            merged_arr[i] = arrB[b]
-            b += 1
-        elif b >= len(arrB):
-            merged_arr[i] = arrA[a]
-            a += 1
-        elif arrA[a] < arrB[b]:
-            merged_arr[i] = arrA[a]
-            a += 1
-        else:
-            merged_arr[i] = arrB[b]
-            b += 1
-    # merge sorted arrays
-
-    # 1. While your data set contains more than one item, split it in half
-    # 2. Once you have gotten down to a single element, you have also *sorted* that element
-    #    (a single element cannot be "out of order")
-    # 3. Start merging your single lists of one element together into larger, sorted sets
-    # 4. Repeat step 3 until the entire data set has been reassembled
-
+    
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort(arr):
+def merge_sort( arr ):
     # TO-DO
-    if len(arr) > 1:
-        left = merge_sort(arr[0:len(arr)//2])
-        right = merge_sort(arr[len(arr)//2:])
-        arr = merge(left, right)
 
     return arr
 
